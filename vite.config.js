0ecf9a607d9fd16801ed04ec64017f6b0dc357a5
fxpby <fxpby@outlook.com>
2023-11-29 18:09:33 +0800

    chore: add UnoCSS

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
+import UnoCSS from 'unocss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [UnoCSS(), react()],
   css: {
     // 预处理器配置项
     preprocessorOptions: {
