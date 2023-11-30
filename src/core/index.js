37869499c061c924e59292a3b94f9585e991abd5
fxpby <fxpby@outlook.com>
2023-11-30 14:03:53 +0800

    feat: 抽离 writeXLSXFile 函数

--- src/core/index.js
+++ src/core/index.js
@@ -26,32 +26,40 @@ const processExcelToJson = ({ file }) => {
   })
 }
 
-const processJsonToExcel = ({ file }) => {
-  const { name } = file
+const writeXLSXFile = ({ data, fileName = 'output.xlsx' }) => {
+  // 创建工作簿
+  const workbook = XLSX.utils.book_new()
+  // 创建工作表
+  const worksheet = XLSX.utils.json_to_sheet(data)
+  // 将工作表添加到工作簿中
+  XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1')
+  // 导出 Excel 文件
+  XLSX.writeFile(workbook, fileName)
+}
 
-  return new Promise((resolve, reject) => {
-    if (file instanceof File) {
-      const reader = new FileReader()
+const processJsonToExcel = ({ file, json }) => {
+  if (file) {
+    const { name } = file
 
-      reader.onload = () => {
-        const jsonData = JSON.parse(reader.result)
-        resolve(jsonData)
+    return new Promise((resolve, reject) => {
+      if (file instanceof File) {
+        const reader = new FileReader()
+
+        reader.onload = () => {
+          const jsonData = JSON.parse(reader.result)
+          resolve(jsonData)
+        }
+
+        reader.readAsText(file)
+      } else {
+        reject(new Error('传入的不是 File'))
       }
-
-      reader.readAsText(file)
-    } else {
-      reject(new Error('传入的不是 File'))
-    }
-  }).then((result) => {
-    // 创建工作簿
-    const workbook = XLSX.utils.book_new()
-    // 创建工作表
-    const worksheet = XLSX.utils.json_to_sheet(result)
-    // 将工作表添加到工作簿中
-    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1')
-    // 导出 Excel 文件
-    XLSX.writeFile(workbook, `${name.split('.')[0]}.xlsx`)
-  })
+    }).then((result) => {
+      writeXLSXFile({ data: result, fileName: `${name.split('.')[0]}.xlsx` })
+    })
+  } else {
+    writeXLSXFile({ data: json })
+  }
 }
 
 export { processExcelToJson, processJsonToExcel }
