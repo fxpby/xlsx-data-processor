b807e0bec435f3a5a25ef20bf28ef9e2d9ad42d3
fxpby <fxpby@outlook.com>
2023-11-30 14:02:28 +0800

    feat: add filter columns logic

--- src/components/filter/index.jsx
+++ src/components/filter/index.jsx
@@ -8,11 +8,10 @@ export default function Filter() {
   const { currentRadio } = useChooseDataDisplay()
   const { handleDisplayColumn, currentColumns, handleFilterData } = useCore()
   const input = useRef(null)
-  const displayColumns = useRef(null)
   const displayResult = useRef(null)
 
   const [checkbox, setCheckbox] = useState([])
-  const [filterMap, setFilterMap] = useState([])
+  const [filterColumns, setFilterColumns] = useState([])
   const clickButton = () => {
     if (input.current) {
       input.current.click()
@@ -20,17 +19,25 @@ export default function Filter() {
   }
   const handleChangeCheckbox = (e) => {
     const { checked, name } = e.target
-    setFilterMap((pre) => {
-      console.log('pre: ', pre, currentColumns)
+    setFilterColumns((pre) => {
       if (checked) {
-        const col = currentColumns.filter((x) => pre.includes(x) || x === name)
-        console.log('col: ', col)
-        return col.map((x) => {
-          return {
-            key: x,
-            value: null,
-          }
-        })
+        if (pre.length) {
+          const newCol = [...pre.map((x) => x.key), name]
+          const col = currentColumns.filter((x) => newCol.includes(x))
+          return col.map((x) => {
+            return {
+              key: x,
+              value: null,
+            }
+          })
+        } else {
+          return [
+            {
+              key: name,
+              value: null,
+            },
+          ]
+        }
       } else {
         return pre.filter((x) => x.key !== name)
       }
@@ -38,6 +45,14 @@ export default function Filter() {
   }
   const handleChangeInput = (e) => {
     const { value, name } = e.target
+    const filterKeys = filterColumns.map((x) => x.key)
+
+    if (filterKeys.includes(name)) {
+      setFilterColumns((pre) => {
+        pre.find((x) => x.key === name).value = value
+        return pre
+      })
+    }
   }
   return (
     <div>
@@ -49,15 +64,10 @@ export default function Filter() {
         type="file"
         ref={input}
         name="fileUploader"
-        onChange={(e) =>
-          handleDisplayColumn({
-            e,
-            dom: displayColumns.current,
-          })
-        }
+        onChange={(e) => handleDisplayColumn({ e })}
         accept=".xls, .xlsx"
       />
-      <div ref={displayColumns}>
+      <div>
         {currentColumns.length ? (
           <>
             <div>选择的文件列名如下，请勾选筛选项，未勾选项不做筛选处理</div>
@@ -96,6 +106,7 @@ export default function Filter() {
           handleFilterData({
             dom: displayResult.current,
             downloadable: currentRadio === 'download',
+            filterColumns,
           })
         }>
         开始处理数据
